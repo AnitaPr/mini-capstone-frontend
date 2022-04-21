6260d6f46abf72aa0864a92de0057319a78fc5ad
AnitaPr <anitabllaca88@gmail.com>
2022-04-20 20:58:02 -0400

    Configure Netlify redirects

---
+++ src/views/ProductsIndex.vue
@@ -0,0 +1,34 @@
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      products: [],
+    };
+  },
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("/products").then((response) => {
+        console.log("products index", response);
+        this.products = response.data;
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="products-index">
+    <h1>All Products</h1>
+    <div v-for="product in products" v-bind:key="product.id">
+      <h2>{{ product.name }}</h2>
+      <img v-bind:src="product.url" v-bind:alt="product.name" />
+      <p>Name: {{ product.name }}</p>
+      <p>Price: {{ product.price }}</p>
+      <p>Description: {{ product.description }}</p>
+    </div>
+  </div>
+</template>
